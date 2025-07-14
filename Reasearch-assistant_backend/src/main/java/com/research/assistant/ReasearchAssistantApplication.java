9525dfcdb6da8fc340a2e2dc08848f0ad5305616
AMIT GUNJAN JHA <jamit5492@gmail.com>
2025-07-15 01:56:50 +0530

    first commit

---
+++ Reasearch-assistant_backend/src/main/java/com/research/assistant/ReasearchAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.research.assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReasearchAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReasearchAssistantApplication.class, args);
+	}
+
+}
